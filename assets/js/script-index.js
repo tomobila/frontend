ba5c7d5a9173137590505684f8a724299b477480
mehdil-ws25 <lahraichi.mehdi@gmail.com>
2024-08-18 18:11:56 +0100

    Update script-index.js

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -32,6 +32,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                 start: event.attributes.startDate,
                 end: event.attributes.endDate,
                 resourceId: event.attributes.vehicle.data.id,
+                eventColor: 'red',
             }));
 
             console.log(ressources, events);
@@ -163,9 +164,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
             window.addEventListener('resize', updateAspectRatio);
             updateAspectRatio();
-            setTimeout(() => {
-
-            }, 1000);
             calendar.render();
 
 
@@ -175,5 +173,153 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
 
 
+    // var calendar = new FullCalendar.Calendar(calendarEl, {
+    //     // headerToolbar: {
+    //     //   center: 'dayGridMonth,resourceTimelineWeek' // buttons for switching between views
+    //     // },
+    //     slotLabelFormat: [
+    //         { day: 'numeric' }, // top level of text
+    //         // { weekday: 'short' } // lower level of text
+    //     ],
+    //     chedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    //     headerToolbar: {
+    //         left: 'title',
+    //         center: '',
+    //         right: 'prev,next resourceTimelineMonth,next15Days,next30Days'
+    //     },
+    //     views: {
+    //         next15Days: {
+    //             type: 'resourceTimeline',
+    //             duration: { days: 15 },
+    //             buttonText: '15 days'
+    //         },
+    //         next30Days: {
+    //             type: 'resourceTimeline',
+    //             duration: { days: 30 },
+    //             buttonText: '30 days'
+    //         }
+    //     },
+    //     initialView: 'resourceTimelineMonth',
+    //     aspectRatio: 3.5,
+    //     // initialDate: new Date(),
+    //     // initialDate: new Date().toISOString().split('T')[0],
+    //     editable: true,
+    //     selectable: true,
+    //     resourceAreaHeaderContent: 'Vehicules',
 
+    //     dayCellContent: function (arg) {
+    //         arg.dayNumberText = arg.dayNumberText.replace(/.*(\d{1,2}).*/, '$1');
+    //     },
+    //     windowResize: function (view) {
+    //         console.log('The calendar has adjusted to a window resize');
+    //     },
+    //     resourceLabelContent: function (arg) {
+    //         let resource = arg.resource;
+    //         // console.log(resource)
+    //         let HTLM = `
+    //             <div class="d-flex align-items-center mb-2">
+    //                 <div class="avatar avatar-4by3 align-middle me-3">
+    //                     <img src="${resource.extendedProps.img}" class="avatar-img rounded p-1">
+    //                 </div>
+    //                 <p class="m-0">${resource.title} </p>
+    //                 </div>`
+
+    //         let container = document.createElement('div');
+    //         container.classList.add('d-flex', 'align-items-center');
+
+    //         let container2 = document.createElement('div');
+    //         container2.classList.add('avatar', 'avatar-4by3', 'align-middle', 'me-3');
+
+    //         let img = document.createElement('img')
+    //         img.classList.add('avatar-img', 'rounded', 'p-1')
+    //         img.src = resource.extendedProps.img; // Use the image URL from the resource
+
+    //         let title = document.createElement('p');
+    //         title.classList.add('m-0');
+    //         title.innerText = resource.title;
+
+    //         container2.appendChild(img)
+
+
+    //         container.appendChild(container2);
+    //         container.appendChild(title);
+
+    //         return { domNodes: [container] };
+    //     },
+    //     eventContent: function (arg) {
+
+    //         let event = arg.event;
+    //         let now = new Date();
+    //         let start = event.start;
+    //         let end = event.end;
+
+    //         let progress = 0;
+    //         if (now > start && now < end) {
+    //             progress = ((now - start) / (end - start)) * 100;
+    //             console.log(progress);
+    //         } else if (now >= end) {
+    //             progress = 100;
+    //         }
+
+    //         let customHtml = `
+    //               <div class="fc-event-custom d-flex">
+    //                 <div class="avatar avatar-xs me-3">
+    //                     <img src="assets/img/avatars/profiles/avatar-1.jpg" class="avatar-img rounded-circle" alt="...">
+    //                 </div>
+    //                 <div class="d-flex flex-1 w-100">
+
+    //                   <div class="fc-event-progress-container w-100">
+    //                      <div class="fc-event-title">${event.title}</div>
+    //                     <div class="fc-event-progress-bar bg-white h-2" style="width: ${progress}%" ></div>
+    //                   </div>
+    //                   <span> ${Math.round(progress)}%</span>
+
+    //                 </div>
+    //             </div>
+    //                   `;
+    //         return { html: customHtml };
+
+    //         // <div class="fc-event-time">${event.start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - ${event.end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
+    //     }
+    // });
+
+    // window.addEventListener('resize', updateAspectRatio);
+    // updateAspectRatio();
+    // calendar.render();
+
+
+
+    // fetch(APICars)
+
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error(`HTTP error! status: ${response.status}`);
+    //         }
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         // console.log(data);
+    //         let extractedData = data.data.map(item => ({
+    //             id: item.id,
+    //             title: item.attributes.name,
+    //             img: localhost + item.attributes.mainImage.data.attributes.url // Assuming the main image URL is needed
+    //         }));
+
+    //         // console.log('====================================');
+    //         // console.log(extractedData)
+    //         // console.log('====================================');
+    //         calendar.extractedData
+
+    //         calendar.getResources().forEach(function (resource) {
+    //             resource.remove();
+    //         });
+
+    //         extractedData.forEach(resource => {
+    //             calendar.addResource(resource);
+    //         });
+    //         calendar.render();
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching data:', error);
+    //     });
 })
